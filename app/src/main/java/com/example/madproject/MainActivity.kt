92a54e670580f8503f65f0a70ed5373a40b2ded0
JaneeshaPabasara <IT23599604@my.sliit.lk>
2025-08-22 00:07:09 +0530

    8 commit

--- app/src/main/java/com/example/madproject/MainActivity.kt
+++ app/src/main/java/com/example/madproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        window.decorView.setOnClickListener {
+            val intent = Intent(this,Onboard1::class.java)
+            startActivity(intent)
+           // finish() //Close MainActivity after navigation
+        }
+
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

876b604e3872aa820f2d19d9c46c2aa372441b76
JaneeshaPabasara <IT23599604@my.sliit.lk>
2025-08-31 07:17:32 +0530

    18 commit

--- app/src/main/java/com/example/madproject/PaymentMethod.kt
+++ app/src/main/java/com/example/madproject/PaymentMethod.kt
@@ -3,64 +3,61 @@ package com.example.madproject
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class ProfilePage : AppCompatActivity() {
+class PaymentMethod : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.profilepage)
+        setContentView(R.layout.paymentmethod)
 
-        val bottomNavigation =
-            findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
+        val bottomNavigation = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_home -> {
-                    Log.d("profile page", "Home icon tapped, navigating to home page")
+                    Log.d("Payment Method page", "Home icon tapped, navigating to home page")
                     val intent = Intent(this, Homepage::class.java)
                     startActivity(intent)
                     finish() // Optional: Close CasualPage if you don't want to keep it in the back stack
                     true
                 }
-
                 R.id.nav_search -> {
-                    Log.d("profile page", "search icon tapped, navigating to search page")
+                    Log.d("payment method page", "search icon tapped, navigating to search page")
                     val intent = Intent(this, Homepage::class.java)
                     startActivity(intent)
                     finish()
                     true
                 }
-
                 R.id.nav_cart -> {
-                    Log.d("profile page", "cart icon tapped, navigating to cart page")
+                    Log.d("payment method page", "cart icon tapped, navigating to cart page")
                     val intent = Intent(this, MyCartPage::class.java)
                     startActivity(intent)
                     finish()
                     true
                 }
-
                 R.id.nav_profile -> {
-                    Log.d("profile page", "profile icon tapped, navigating to profile page")
+                    Log.d("payment method page", "profile icon tapped, navigating to profile page")
                     val intent = Intent(this, ProfilePage::class.java)
                     startActivity(intent)
                     finish()
                     true
                 }
-
                 else -> false
             }
         }
-        val rootLayout = findViewById<TextView>(R.id.pay)
-        rootLayout.setOnClickListener {
-            Log.d("MainActivity", "Screen tapped, navigating to Onboard1")
-            val intent = Intent(this, Onboard1::class.java)
+        val btnNavigate=findViewById<TextView>(R.id.next_button)
+        btnNavigate.setOnClickListener {
+            Log.d("payment method page", "save button clicked, navigating to profile page")
+            val intent = Intent(this, ProfilePage::class.java)
             startActivity(intent)
-            finish()
-
         }
-    }}
+    }
+}
 
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
